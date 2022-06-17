94aa37a9268387ac6d6360fcd5ef5e1c991efc76
Francesco_Cina <mr.francesco.cina@gmail.com>
2022-06-17 13:48:45 +0200

    Add simple contract call from UI

--- lottery/ui-react/src/App.tsx
+++ lottery/ui-react/src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from "./web3";
+import { Lottery__factory } from './generated/abi';
+
+const CONTRACT_ADDRESS = "0x3a70aec91db562eaa73a71055ccaeb90494ca3cb";
 
 function App() {
   web3.listAccounts().then(console.log);
+  const lottery = Lottery__factory.connect(CONTRACT_ADDRESS, web3);
+  lottery.getPlayers().then(players => console.log(`Found ${players.length} players`));
   return (
     <div className="App">
       <header className="App-header">
